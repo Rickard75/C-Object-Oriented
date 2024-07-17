104f4dfee31816f6579990262642734e3649f985
Rickard75 <riccardo.carroccio@studenti.unipd.it>
2024-07-17 15:08:24 +0200

    Removed sums, mean, rms variables and functions

--- particleHist_v4/LifetimeFit.h
+++ particleHist_v4/LifetimeFit.h
@@ -17,8 +17,6 @@ class LifetimeFit {
   void compute();                   // compute mean and rms
 
   unsigned int get_nAcc() const;                // return number of accepted events
-  double get_mean() const;                // return mean mass
-  double get_rms() const;                 // return rms  mass
   double get_min() const;
 
  private:
@@ -27,12 +25,6 @@ class LifetimeFit {
   double max_mass; // max mass
 
   unsigned int nAcc; // number of accepted events
-  double sum_mass; // sum of masses
-  double sum_sqr; // sum of masses square
-
-  double mean; // mean mass
-  double rms; // rms  mass
-
 };
 
 #endif
