988139fb30320c659ca7f196d96d460de8ba99b3
Rickard75 <riccardo.carroccio@studenti.unipd.it>
2024-07-20 19:39:19 +0200

    Checked - to do: compute()

--- particleHist_v5/LifetimeFit.cc
+++ particleHist_v5/LifetimeFit.cc
@@ -1,17 +1,22 @@
 #include "LifetimeFit.h"
 #include "Event.h"
 #include "ParticleReco.h"
+#include "ProperTime.h"
 
 #include <cmath>
 
 double mass( const Event& ev );
 
 // constructor
-LifetimeFit::LifetimeFit( double min, double max ):
+LifetimeFit::LifetimeFit( double min, double max, double min_time, double max_time, double min_scan, double max_scan, double scan_step ):
  // initializations
  min_mass(min),
  max_mass(max),
- nAcc(0) {
+ min_time(min_time),
+ max_time(max_time),
+ min_scan(min_scan),
+ max_scan(max_scan),
+ scan_step(scan_step) {
 }
 
 // destructor
@@ -22,11 +27,15 @@ LifetimeFit::~LifetimeFit() {
 bool LifetimeFit::add( const Event& ev ) {
   // since mass.cc is now encapsulated in ParticleReco, we can call the get_mass() method
   static ParticleReco* pr = ParticleReco::instance();
+  static ProperTime* pt = ProperTime::instance();
   double my_mass = pr->get_mass();
+  double my_time = pt->get_time();
   // check for mass being in range
   if (min_mass <= my_mass && my_mass <= max_mass){
-    nAcc++;
-    return true;
+    if(min_time <= my_time && my_time <= max_time){
+      times.push_back(my_time);
+      return true;
+    }
   }
   return false;
 
@@ -36,10 +45,11 @@ bool LifetimeFit::add( const Event& ev ) {
 void LifetimeFit::compute() {
 }
 
+// CAN THIS BE DELTED?????????????????????????????????
 // return number of selected events
-unsigned int LifetimeFit::get_nAcc() const{
-  return nAcc;
-}
+//unsigned int LifetimeFit::get_nAcc() const{
+//  return nAcc;
+//}
 
 /*
 The following function is inside Singleton.h:
--- particleHist_v5/LifetimeFit.h
+++ particleHist_v5/LifetimeFit.h
@@ -1,6 +1,7 @@
 #ifndef LifetimeFit_h
 #define LifetimeFit_h
 
+#include <vector>
 class Event;
 
 class LifetimeFit {
@@ -8,21 +9,31 @@ class LifetimeFit {
  public:
 
   // CONSTRUCTOR
-  LifetimeFit( double min, double max ); // mass range
-
-  // DESTRUCTOR
+  LifetimeFit( double min, double max, double min_time, double max_time, double min_scan, double max_scan, double scan_step ); // mass range
   ~LifetimeFit();
 
   bool add( const Event& ev );      // add data from a new event
   void compute();                   // compute mean and rms
 
-  unsigned int get_nAcc() const;                // return number of accepted events
+  
+  unsigned int get_nAcc() const;    // CAN THIS BE DELETED????????????????
+  double get_lifeTime() const;      // return mean lifetime
+  double get_lifeTimeError() const; // return error on mean lifetime
+ 
  private:
 
-  double min_mass; // min mass
-  double max_mass; // max mass
+  double min_mass; 
+  double max_mass; 
+  double min_time; // min time
+  double max_time; // max time
+  double min_scan; // min scan
+  double max_scan; // max scan
+  double scan_step; // scan step
+  std::vector<double> times; // container for times
+  double particle_lifetime_mean; // lifetime mean
+  double particle_lifetime_rms; // lifetime rms
 
-  unsigned int nAcc; // number of accepted events
+
 };
 
 #endif
