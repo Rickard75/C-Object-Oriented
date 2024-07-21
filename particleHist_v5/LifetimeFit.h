fb9facab237515e0abaf31bb52efc78c8cda229a
Rickard75 <riccardo.carroccio@studenti.unipd.it>
2024-07-21 12:57:56 +0200

    Checked

--- particleHist_v5/LifetimeFit.h
+++ particleHist_v5/LifetimeFit.h
@@ -3,14 +3,17 @@
 
 #include <vector>
 class Event;
+class ParticleReco;
 
 class LifetimeFit {
 
  public:
 
-  // CONSTRUCTOR
-  LifetimeFit( double min, double max, double min_time, double max_time, double min_scan, double max_scan, double scan_step ); // mass range
-  ~LifetimeFit();
+  
+  LifetimeFit( double min, double max, 
+               double min_time, double max_time, 
+               double min_scan, double max_scan, double scan_step ); // constructor
+  ~LifetimeFit(); // destructor
 
   bool add( const Event& ev );      // add data from a new event
   void compute();                   // compute mean and rms
@@ -22,12 +25,12 @@ class LifetimeFit {
  
  private:
 
-  double min_mass; 
+  double min_mass; // mass range
   double max_mass; 
-  double min_time; // min time
-  double max_time; // max time
-  double min_scan; // min scan
-  double max_scan; // max scan
+  double min_time; // time range
+  double max_time; 
+  double min_scan; // scan range
+  double max_scan;
   double scan_step; // scan step
   std::vector<double> times; // container for times
   double particle_lifetime_mean; // lifetime mean
